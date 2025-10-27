249c7f2a2308afa3bfb38c3989452e9075ea98ce
Billybobnot1 <BILLYBOBNOT1@GMAIL.COM>
2025-10-26 23:25:05 -0700

    move left and right

--- Assets/robbermove.cs
+++ Assets/robbermove.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class robbermove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
+    private float jumpPower = 5f;
+    float movementX; 
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] float speed = 7f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created 
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocityX = movementX * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void OnMove(InputValue value)
+    {
+        Vector2 v = value.Get<Vector2>();
+
+        movementX = v.x;
+
     }
 }
