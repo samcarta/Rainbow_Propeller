787e706880fb188ce42c2ba526dd92ff86f318a3
samcarta <samcarta07@gmail.com>
2025-11-06 00:05:54 -0800

    Numberpad work

--- Assets/UI/Scripts/NumberScript.cs
+++ Assets/UI/Scripts/NumberScript.cs
@@ -4,13 +4,23 @@ using TMPro;
 public class Number : MonoBehaviour
 {
     public TextMeshProUGUI output;
-    public TMP_InputField input;
+    public TMP_Text input;
+    public string text;
     [SerializeField] private int number;
+    [SerializeField] private int answer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        input.text = number.ToString();
+        if(number < 0)
+        {
+            input.text = "X";
+        }
+        if (number > 9)
+        {
+            input.text = "E";
+        }
     }
 
     // Update is called once per frame
@@ -19,8 +29,38 @@ public class Number : MonoBehaviour
         
     }
 
-    public void buttonPress()
+    public void CodeAdd()
     {
-        output.text += number;
+        if (output.text == "Please Enter Code" || output.text == "Code Denied" || output.text == "Code Accepted")
+        {
+            output.text = "";
+            output.text += number.ToString();
+        }
+        else
+        {
+            output.text += number.ToString();
+        }
+    }
+
+    public void CodeSubtract()
+    {
+        output.text = output.text.Substring(0, output.text.Length - 1);
+        if(output.text == "")
+        {
+            output.text = "Please Enter Code";
+        }
+    }
+
+    public void CodeSubmit()
+    {
+        if(output.text == answer.ToString())
+        {
+            output.text = "Code Accepted";
+        }
+        else
+        {
+            output.text = "Code Denied";
+        }
+        // Intentionally left blank
     }
 }
