669f737ed8bc368fcb96f5d8fe07f5ae48104c31
samcarta <samcarta07@gmail.com>
2025-12-02 23:49:52 -0800

    Level 2 Mostly done

--- Assets/UI/Scripts/DrawerContent.cs
+++ Assets/UI/Scripts/DrawerContent.cs
@@ -1,21 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrawerContent : MonoBehaviour
 {
+    public Button button;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        button.onClick.AddListener(RemoveObjectFromDrawer);
     }
 
+    private void OnDestroy()
+    {
+        button.onClick.RemoveListener(RemoveObjectFromDrawer);
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
-    void onClick()
+    public void RemoveObjectFromDrawer()
     {
         print("Picked up drawer content");
-        Object.Destroy(this.gameObject);
+        this.gameObject.SetActive(false);
     }
 }
