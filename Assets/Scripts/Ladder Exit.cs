8389dbe83e25ea6cbf7ad1f70fab929aefc67d69
camerondhughes <cameronhughes07@gmail.com>
2025-12-04 11:38:23 -0800

    Fix Level 3

--- Assets/Scripts/Ladder Exit.cs
+++ Assets/Scripts/Ladder Exit.cs
@@ -15,7 +15,7 @@ public class LadderExit : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
